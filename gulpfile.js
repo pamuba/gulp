8d532535e0c89559f93fa361ec417c669a525057
pamuba <pamuba@users.noreply.github.com>
2020-08-05 19:46:58 +0530

    1

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
-var rename = require('gulp-rename')
+var rename = require('gulp-rename');
+var sass = require( 'gulp-sass' )
 
-var styleSRC = './src/scss/style.css';
+
+var styleSRC = './src/scss/style.scss';
 var styleDIST = './dist/css/'
 
 gulp.task('style', function(done){
     gulp.src( styleSRC )
+        .pipe( sass({
+            errorLoToConsole:true,
+            outputStyle: 'compressed'
+        }))
+        .on('error', console.error.bind( console ))
         .pipe(rename({ suffix:'.min' }))
         .pipe(gulp.dest(styleDIST))
         done();
