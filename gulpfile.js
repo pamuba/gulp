e76ca2c72bf6046618ca7c27d564676df15e2c3e
pamuba <pamuba@users.noreply.github.com>
2020-08-05 20:06:53 +0530

    3

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
-var sass = require( 'gulp-sass' )
-var autoprefixer = require('gulp-autoprefixer')
+var sass = require( 'gulp-sass' );
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 
 var styleSRC = './src/scss/style.scss';
@@ -9,13 +10,16 @@ var styleDIST = './dist/css/'
 
 gulp.task('style', function(done){
     gulp.src( styleSRC )
+        .pipe( sourcemaps.init() )
         .pipe( sass({
             errorLoToConsole:true,
             outputStyle: 'compressed'
         }))
         .on('error', console.error.bind( console ))
-        .pipe( autoprefixer({ browsers: ['last 2 versions']}))
+        // .pipe( autoprefixer({ browsers: ['last 2 versions']}))
+        .pipe( autoprefixer())
         .pipe(rename({ suffix:'.min' }))
+        .pipe( sourcemaps.write('./') )
         .pipe(gulp.dest(styleDIST))
         done();
 })
